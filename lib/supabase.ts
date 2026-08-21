e730c65404106a5b462c98a351d701bcb01c9763
CAP-Mantenimiento <hector.aguilar@cap.edu.mx>
2026-08-20 20:30:48 -0600

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ujirmnokxcuyhkwdhted.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_7pKU6_eloYtapHo5bmAwFw_6nFAFx7w';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  'https://ujirmnokxcuyhkwdhted.supabase.co',
+  'sb_publishable_7pKU6_eloYtapHo5bmAwFw_6nFAFx7w'
+);
